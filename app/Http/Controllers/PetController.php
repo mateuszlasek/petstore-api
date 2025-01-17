68131b0babf42c6f82c55f15097139f0b092bc55
mateuszlasek <mateusz.lasek17@wp.pl>
2025-01-17 10:08:02 +0100

    add index function

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ApiService;
 use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
-    //
+    public function __construct(ApiService $apiService)
+    {
+        $this->apiService = $apiService;
+    }
+
+    public function index()
+    {
+        $pets = $this->apiService->findPetsByStatus('available');
+
+        if (is_null($pets)) {
+            return redirect()->route('pets.index')->withErrors(['error' => 'Failed to fetch pets.']);
+        }
+
+        return view('pets.index', compact('pets'));
+    }
 }
