4cf2ce3b91f8da23e438560282a9bf21e8ff884d
mateuszlasek <mateusz.lasek17@wp.pl>
2025-01-17 10:39:08 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PetController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PetController::class, 'index'])->name('pets.index');
+Route::get('/create', [PetController::class, 'create'])->name('pets.create');
+Route::get('/edit/{id}', [PetController::class, 'edit'])->name('pets.edit');
+Route::post('/store', [PetController::class, 'store'])->name('pets.store');
+Route::put('/update/{id}', [PetController::class, 'update'])->name('pets.update');
+Route::delete('/delete/{id}', [PetController::class, 'destroy'])->name('pets.destroy');
